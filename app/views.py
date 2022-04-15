57e7c2770e99628607788f8aa8005978e40922a0
Manoj <manoj.jawalkar@in.ibm.com>
2022-04-15 17:00:17 +0530

    removal of unused code #1

--- app/views.py
+++ app/views.py
@@ -45,35 +45,7 @@ def get_ariel_databases():
 @viewsbp.route('/network_search', methods=['GET','POST'])
 def network_search():
     if request.method == 'GET':
-        try:
-            networks = qpylib.REST('get', '/api/config/network_hierarchy/networks')
-            for network in networks.json():
-                qpylib.log("Networks ="+ str(network))
-                net_name = network['name']
-                net_cidr = network['cidr']
-                net_group = network['group']
-
-            qpylib.log("Name="+ net_name)
-            qpylib.log("cidr="+ net_cidr)
-            qpylib.log("group="+ net_group)
-            qpylib.log(type(net_name))
-            qpylib.log(type(net_cidr))
-
-            qpylib.log(type(net_group))
-            base_url = qpylib.get_app_base_url()
-            qpylib.log("base URL = "+base_url)
-            qpylib.log(url_for('views.network_search'))
-            y = IPv4Network(net_cidr)
-            qpylib.log("--- printing IPv4 addr ---")
-            qpylib.log(y)
-            z = IPv4Network('0.0.0.14')
-            qpylib.log(y.overlaps(z))
-
-            return render_template("network_search.html", title="Admin Me!")
-
-        except Exception as ex:
-            qpylib.log("This is an exception"+str(ex),'ERROR')
-            raise
+        return render_template("network_search.html", title="Admin Me!")
     else:
         found = {}
         ip = request.form["ip"]
